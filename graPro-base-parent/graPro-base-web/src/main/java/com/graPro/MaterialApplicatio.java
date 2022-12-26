573f73ce90cc47cb1cb8ef8e7132e60e1d82f2c8
nifeng <69586965+Tiemoremilk@users.noreply.github.com>
2022-12-26 14:18:46 +0800

    CORS

---
+++ graPro-base-parent/graPro-base-web/src/main/java/com/graPro/MaterialApplicatio.java
@@ -0,0 +1,12 @@
+package com.graPro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialApplicatio {
+    public static void main(String[] args){
+        SpringApplication.run (MaterialApplicatio.class,args);
+    }
+}
